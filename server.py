4bd4eef873af07977ed31b97df428da5af8c03e7
Joel Choppavarapu <choppavarapujoel@gmail.com>
2025-09-14 00:09:01 +0700

    Fix port for Railway deploy

--- server.py
+++ server.py
@@ -53,4 +53,6 @@ def download_file(filename):
     return send_from_directory(RECORDINGS_DIR, filename, as_attachment=False)
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5003, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
