661796035ec81fad5659435009080a69390eb0f7
Joel Choppavarapu <choppavarapujoel@gmail.com>
2025-09-14 16:00:21 +0530

    Fix Railway deployment with gunicorn + eventlet

--- server.py
+++ server.py
@@ -65,7 +65,8 @@ def download_file(filename):
 # ------------------- Run App -------------------
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # Railway injects PORT
     socketio.run(app, host="0.0.0.0", port=port)
+
 
 
